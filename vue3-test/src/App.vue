7f95dcb454d3eb3691ea280a055130aed99f8d76
ljk4268 <ljk4268@kakao.com>
2022-11-12 17:31:20 +0900

    템플릿 문법 연습

--- vue3-test/src/App.vue
+++ vue3-test/src/App.vue
@@ -1,48 +1,29 @@
 <template>
-  <h1 @click="increase">
-    {{ count }}
+  <h1 :[attr]="'actiive'">
+    {{ msg }}
   </h1>
-  <div v-if="count > 4">
-    4보다 큽니다!
-  </div>
-  <ul>
-    <Fruit
-      v-for="fruit in fruits"
-      :key="fruit"
-      :name="fruit">
-      {{ fruit }}
-    </Fruit>
-  </ul>
 </template>
 
 <script>
-import Fruit from './components/MultiFruits.vue'
 export default {
-  components: {
-    Fruit
-  },
   data(){
     return {
-      count: 0,
-      fruits: ['apple','banana','cherry']
+      msg: 'active',
+      attr: 'class',
+      event: 'click'
     }
   },
   methods: {
-    increase(){
-      this.count += 1;
+    add(){
+      this.msg += '!'
     }
   }
 }
 </script>
 
 <style lang="scss">
-  h1 {
-    font-size: 50px;
+  .active{
     color: royalblue;
-  }
-  ul {
-    li {
-      font-size: 40px;
-    }
+    font-size: 100px;
   }
 </style>
