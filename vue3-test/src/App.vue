3ff38fc17c248123bd1cead6f559be9a1ef03c69
ljk4268 <ljk4268@kakao.com>
2022-11-12 16:01:11 +0900

    vue v-if v-for

---
+++ vue3-test/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1 @click="increase">
+    {{ count }}
+  </h1>
+  <div v-if="count > 4">
+    4보다 큽니다!
+  </div>
+  <ul>
+    <Fruit
+      v-for="fruit in fruits"
+      :key="fruit"
+      :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
+</template>
+
+<script>
+import Fruit from './components/MultiFruits.vue'
+export default {
+  components: {
+    Fruit
+  },
+  data(){
+    return {
+      count: 0,
+      fruits: ['apple','banana','cherry']
+    }
+  },
+  methods: {
+    increase(){
+      this.count += 1;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  h1 {
+    font-size: 50px;
+    color: royalblue;
+  }
+  ul {
+    li {
+      font-size: 40px;
+    }
+  }
+</style>
