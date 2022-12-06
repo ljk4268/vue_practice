5eec706299353049ab96f2392ebb6360ca0061ae
ljk4268 <ljk4268@kakao.com>
2022-12-06 20:59:08 +0900

    클래스와 스타일 바인딩
    HTML class, style에 데이터로 스타일 바인딩 하는 방법

--- vue3-webpack-template/src/App.vue
+++ vue3-webpack-template/src/App.vue
@@ -1,44 +1,31 @@
 <template>
-  <button @click="add">ADD</button>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  
+  <h1
+    :style="[fontStyle, backgroundStyle]"
+    @click="changeStyle"
+  >
+  Hello?!
+  </h1>
 </template>
 
 <script>
 export default {
   data(){
     return {
-      // Getter, Setter
-      msg:'Hello Computed!'
-    }
-  },
-  computed: {
-    // 이미 계산이 된 데이터 : 캐싱이 됨.
-    // 최적화에 많이 사용됨. 
-    // 값을 얻어내는 용도 : Getter
-    // 계산된 부분에 Setter 적용
-    // reversedMessage(){
-    //   return this.msg.split('').reverse().join('')
-    // }
-
-    reversedMessage: {
-      get(){
-        return this.msg.split('').reverse().join('')
+      fontStyle: {
+        color: 'orange',
+        fontSize: '30px'
       },
-      set(newValue){
-        console.log(newValue)
+      backgroundStyle: {
+        backgroundColor: 'black'
       }
     }
-
   },
   methods: {
-    add(){
-      this.reversedMessage += '!?'
+    changeStyle(){
+      this.fontStyle.color = 'red'
+      this.fontStyle.fontSize = '50px'
     }
   }
-
 }
 </script>
+
