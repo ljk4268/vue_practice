e5480e732459356fdc0a1099900f3fd1d32f4aaf
ljk4268 <ljk4268@kakao.com>
2022-12-06 19:28:18 +0900

    computed
    computed 기능을 사용해서, 계산된 데이터를 캐싱하여 데이터바인딩을 하면 성능 최적화에 도움이 된다.

--- vue3-webpack-template/src/App.vue
+++ vue3-webpack-template/src/App.vue
@@ -1,13 +1,31 @@
 <template>
-  <Fruits/>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  
 </template>
 
 <script>
-import Fruits from './components/Fruits.vue'
-
 export default {
-  components: {
-    Fruits
+  data(){
+    return {
+      msg:'Hello Computed!'
+    }
+  },
+  computed: {
+    // 이미 계산이 된 데이터 : 캐싱이 됨.
+    // 최적화에 많이 사용됨. 
+    reversedMessage(){
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  methods: {
+    // 실행 될때마다 계산되는 코드
+    reverseMessage(){
+      return this.msg.split('').reverse().join('')
+    }
   }
+
 }
 </script>
