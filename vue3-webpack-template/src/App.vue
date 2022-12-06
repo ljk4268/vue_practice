fb6770f920b439661939ed84064d7b39fd641d84
ljk4268 <ljk4268@kakao.com>
2022-12-06 19:58:38 +0900

    computed Getter,Setter
    Getter : computed는 이미 계산된 데이터를 읽는 용도로 사용되는 메소드
    Setter : 계산된 부분에 수정이 필요한 경우 사용됨

--- vue3-webpack-template/src/App.vue
+++ vue3-webpack-template/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <button @click="add">ADD</button>
   <h1>{{ reversedMessage }}</h1>
   <h1>{{ reversedMessage }}</h1>
   <h1>{{ reversedMessage }}</h1>
@@ -10,20 +11,32 @@
 export default {
   data(){
     return {
+      // Getter, Setter
       msg:'Hello Computed!'
     }
   },
   computed: {
     // 이미 계산이 된 데이터 : 캐싱이 됨.
     // 최적화에 많이 사용됨. 
-    reversedMessage(){
-      return this.msg.split('').reverse().join('')
+    // 값을 얻어내는 용도 : Getter
+    // 계산된 부분에 Setter 적용
+    // reversedMessage(){
+    //   return this.msg.split('').reverse().join('')
+    // }
+
+    reversedMessage: {
+      get(){
+        return this.msg.split('').reverse().join('')
+      },
+      set(newValue){
+        console.log(newValue)
+      }
     }
+
   },
   methods: {
-    // 실행 될때마다 계산되는 코드
-    reverseMessage(){
-      return this.msg.split('').reverse().join('')
+    add(){
+      this.reversedMessage += '!?'
     }
   }
 
