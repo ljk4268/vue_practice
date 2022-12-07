27feeb609849de0cdd355bbd7c7c6a5a33df1013
ljk4268 <ljk4268@kakao.com>
2022-12-07 16:47:25 +0900

    조건부 렌더링(v-if vs v-show)
    조건이 변경되지 않는 경우 v-if
    자주 전환이 필요한 경우 v-show (css만으로 전환이 되는거니까!)

--- vue3-webpack-template/src/App.vue
+++ vue3-webpack-template/src/App.vue
@@ -1,29 +1,42 @@
 <template>
-  <h1
-    :style="[fontStyle, backgroundStyle]"
-    @click="changeStyle"
-  >
-  Hello?!
+  <button @click="handler">
+    Click me!
+  </button>
+  <h1 v-show="isShow">
+    Hello?!
   </h1>
+  <!-- isShow:true이면 보이고 -->
+  <!-- isShow:false면 안보인다 -->
+  <!-- toggle내용 -->
+  <!-- <h1 v-if="isShow">
+    Hello?!
+  </h1>
+  <h1 v-else-if="count > 3">
+    Count > 3
+  </h1>
+  <h1 v-else>
+    Good!
+  </h1> -->
+  <!-- <template v-if="isShow">
+    <h1>Title</h1>
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+  </template> -->
+  
 </template>
 
 <script>
 export default {
   data(){
     return {
-      fontStyle: {
-        color: 'orange',
-        fontSize: '30px'
-      },
-      backgroundStyle: {
-        backgroundColor: 'black'
-      }
+      isShow: false,
+      count: 0
     }
   },
   methods: {
-    changeStyle(){
-      this.fontStyle.color = 'red'
-      this.fontStyle.fontSize = '50px'
+    handler(){
+      this.isShow = !this.isShow
+      this.count += 1
     }
   }
 }
