6824b617759a43a2b9f78868058fa0911a01227e
ljk4268 <ljk4268@kakao.com>
2022-12-07 17:24:52 +0900

    리스트렌더링( v-for )

--- vue3-webpack-template/src/App.vue
+++ vue3-webpack-template/src/App.vue
@@ -2,41 +2,36 @@
   <button @click="handler">
     Click me!
   </button>
-  <h1 v-show="isShow">
-    Hello?!
-  </h1>
-  <!-- isShow:true이면 보이고 -->
-  <!-- isShow:false면 안보인다 -->
-  <!-- toggle내용 -->
-  <!-- <h1 v-if="isShow">
-    Hello?!
-  </h1>
-  <h1 v-else-if="count > 3">
-    Count > 3
-  </h1>
-  <h1 v-else>
-    Good!
-  </h1> -->
-  <!-- <template v-if="isShow">
-    <h1>Title</h1>
-    <p>Paragraph 1</p>
-    <p>Paragraph 2</p>
-  </template> -->
+  <ul>
+    <li
+      v-for="{id, name} in newFruits"
+      :key="id">
+      {{ name }} - {{ id }}
+    </li>
+  </ul>
   
 </template>
 
 <script>
+import shortid from 'shortid'
+
 export default {
   data(){
     return {
-      isShow: false,
-      count: 0
+      fruits: ['Apple','Banana','Cherry'],
+    }
+  },
+  computed: {
+    newFruits(){
+      return this.fruits.map( fruit => ({
+        id: shortid.generate(),
+        name: fruit
+      }))
     }
   },
   methods: {
     handler(){
-      this.isShow = !this.isShow
-      this.count += 1
+      this.fruits.push('Orange')
     }
   }
 }
