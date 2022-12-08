9a3822591e7a5bfef9a07ac30dc9df3a16aa8650
ljk4268 <ljk4268@kakao.com>
2022-12-08 12:27:10 +0900

    v-model/폼입력바인딩
    1. 단방향 / 양방향 데이터 바인딩
    2. v-model
    3. v-model 수식어

--- vue3-webpack-template/src/App.vue
+++ vue3-webpack-template/src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <!-- <button @click="handler('hi',$event)">
-    Click 1
-  </button>
-  <button @click="handler('what', $event)">
-    Click 2
-  </button> -->
-  <button @click="handlerA(),handlerB()">
-    click
-  </button>
+  <h1>{{ msg }}</h1>
+  <input 
+    type="text" 
+    v-model.trim="msg" />
 </template>
 
 <script>
 
 export default {
-  methods: {
-    // handler(msg, event){
-    //   console.log(msg)
-    //   console.log(event)
-    // }
-    handlerA(){
-      console.log('A')
-    },
-    handlerB(){
-      console.log('B')
+  data(){
+    return{
+      msg: 'Hello world!'
+    }
+  },
+  watch: {
+    msg(){
+      console.log(this.msg)
     }
   }
 }
 </script>
+
 
