97cc33653bce21bf41a07ec275a21f3a43bc8e66
ljk4268 <ljk4268@kakao.com>
2022-12-15 20:34:47 +0900

    Header-Logo및 Google Font

--- vue3-movie-app/src/components/Header.vue
+++ vue3-movie-app/src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <header>
+    <Logo />
     <div class="nav nav-pills">
       <div 
         v-for="nav in navigations" 
@@ -17,7 +18,11 @@
 </template>
 
 <script>
+import Logo from './Logo.vue'
 export default {
+  components:{
+    Logo
+  },
   data(){
     return {
       navigations: [
@@ -38,3 +43,16 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+header {
+  height: 70px;
+  padding: 0 40px;
+  display: flex;
+  align-items: center;
+  .logo {
+    margin-right: 40px;
+  }
+}
+
+</style>
---
+++ vue3-movie-app/src/components/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+  <RouterLInk 
+    to="/" 
+    class="logo">
+    <span>OMDbAPI</span>.com
+  </RouterLInk>
+</template>
+
+<style lang="scss" scoped>
+@import "../scss/main.scss";
+
+.logo {
+  font-family: "Oswald", sans-serif;
+  font-size: 20px;
+  color: $black;
+  span {
+    color: $primary;
+  }
+}
+</style>
