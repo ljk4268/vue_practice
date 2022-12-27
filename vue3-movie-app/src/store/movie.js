6550e51dc7b4324b222ec240300c0b038d3c138c
ljk4268 <ljk4268@kakao.com>
2022-12-27 16:44:17 +0900

    영화 검색 추가 요청!

--- vue3-movie-app/src/store/movie.js
+++ vue3-movie-app/src/store/movie.js
@@ -21,15 +21,32 @@ export default {
     }
   },
   actions: {
-    async searchMovies({commit}, payload){
-      const { title, type, year } = payload
+    async searchMovies({state, commit}, payload){
+      const { title, type, number, year } = payload
       const OMDB_API_KEY = '7035c60c'
 
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
-      const { Search } = res.data
+      const { Search, totalResults } = res.data
       commit('updateState', {
         movies: Search
       })
+      console.log(totalResults) 
+      console.log(typeof totalResults)
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10)
+
+      // 추가 요청 전송! 
+      if ( pageLength > 1 ){
+        for ( let page = 2; page <= pageLength; page ++ ){
+          if ( page > (number / 10) ) break
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+          const { Search } = res.data
+          commit('updateState',{
+            movies: [...state.moives, ...Search]
+          })
+        }
+      }
     }
   },
 }
