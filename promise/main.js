4f7ed7fd6835ee2663202b4815da2ec64cfddfe8
ljk4268 <ljk4268@kakao.com>
2022-12-28 13:55:34 +0900

    API 비동기처리 연습

--- promise/main.js
+++ promise/main.js
@@ -1,31 +1,33 @@
-function a(number){
-  return new Promise((resolve,reject) => {
-    if ( number > 4 ){
-      return reject()
+function fetchMoives(title) {
+  const OMDB_API_KEY = '7035c60c123'
+  return new Promise(async (resolve, reject)=>{
+    try {
+      const res = await axios.get(`https://omdbapi.com?apikey=${OMDB_API_KEY}&s=${title}`)
+      // 이행 : 연산이 성공적으로 완료됨
+      resolve(res)
+    } catch(error) {
+      console.log(error)
+      // 거부 : 연산이 실패함
+      reject('에러가났어요')
     }
-    setTimeout(()=>{
-      console.log('A')
-      resolve()
-    },1000)
   })
 }
 
-async function test(){
-  // a(1)
-  //   .then(() => {
-  //     console.log('Resolve!')
-  //   })
-  //   .catch(() => {
-  //     console.log('Reject!')
-  //   })
-  //   .finally(() => {
-  //     console.log('Done!')
-  //   })
+async function test() {
   try {
-    await a(8)
-    console.log('Resolve!')
-  } catch(error){
-    console.log('Reject!')
+    const res = await fetchMoives('frozen')
+    console.log(res)
+  } catch (heropy) {
+    console.log(heropy)
   }
+  
 }
+
 test()
+
+function hello() {
+  fetchMoives('jobs')
+    .then(res => console.log(res))
+    .catch(heropy=>{ console.log(heropy)})
+  
+}
