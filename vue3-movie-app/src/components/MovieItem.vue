1131d9681d854c669fe8828e713105db0db1ec71
ljk4268 <ljk4268@kakao.com>
2022-12-28 15:08:34 +0900

    영화아이템 - 기본 출력

--- vue3-movie-app/src/components/MovieItem.vue
+++ vue3-movie-app/src/components/MovieItem.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>{{ movie.imdbID }}</div>
+  <div
+    :style="{ backgroundImage: `url(${movie.Poster}` }"
+    class="movie">
+    <div class="info">
+      <div class="year">
+        {{ movie.Year }}
+      </div>
+      <div class="title">
+        {{ movie.Title }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,3 +23,29 @@ export default {
   }
 }
 </script> 
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+
+  .movie {
+    $width: 200px;
+    width: $width;
+    height: $width * 3/2;
+    margin: 10px;
+    border-radius: 4px;
+    background-color: $gray-400;
+    background-size: cover;
+    overflow: hidden;
+    position: relative;
+    .info {
+      background-color: rgba($black, .3);
+      width: 100%;
+      padding: 14px;
+      font-size: 14px;
+      text-align: center;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+    }
+  }
+</style>
