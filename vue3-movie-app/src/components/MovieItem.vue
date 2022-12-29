8a63e374d20c6cd5db32aecdf2a4e6d32e3201db
ljk4268 <ljk4268@kakao.com>
2022-12-29 17:44:02 +0900

    About 페이지

--- vue3-movie-app/src/components/MovieItem.vue
+++ vue3-movie-app/src/components/MovieItem.vue
@@ -1,5 +1,6 @@
 <template>
-  <div
+  <RouterLink
+    :to="`/movie/${movie.imdbID}`"
     :style="{ backgroundImage: `url(${movie.Poster}` }"
     class="movie">
     <Loader 
@@ -14,7 +15,7 @@
         {{ movie.Title }}
       </div>
     </div>
-  </div>
+  </RouterLink>
 </template>
 
 <script>
--- vue3-movie-app/src/store/about.js
+++ vue3-movie-app/src/store/about.js
@@ -1,1 +1,13 @@
 // 개발자 정보 사용자 정보에 대한 데이터 
+export default {
+  // store에 하나의 모듈일 될 수 있게 해주는 속성
+  namespaced: true,
+  // 참조형 데이터인 객체. 데이터의 불변성을 유지하려면 함수로 만들어서 데이터 반환
+  state: () => ({
+    name: 'HEROPY',
+    email: 'thesecon@gmail.com',
+    blog: 'https://heropy.blog',
+    phone: '+82-10-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
